19daccda27f23102e1ce48c7bb659ef901f39b6b
Ganushkevych <ganushkevych@gmail.com>
2023-10-19 11:01:49 +0300

    1 commit(lab2)

--- lab2/src/main/java/Matrix.java
+++ lab2/src/main/java/Matrix.java
@@ -1,2 +1,11 @@
 public class Matrix {
+    private final int rows;
+    private final int columns;
+    private double[][] numbers;
+
+    public Matrix(int rows, int columns, double[][] numbers) {
+        this.rows = rows;
+        this.columns = columns;
+        this.numbers = numbers;
+    }
 }
